4b7d4ae97e98d4d0196168e23fede3954f4c394c
KodingCore <kcorvais@gmail.com>
2023-05-22 10:28:34 +0000

    suite avec petit jeu pas fini

--- exercice-variables/index.js
+++ exercice-variables/index.js
@@ -1,5 +1,9 @@
+//Exercice 0
+
 const character = "Legolas";
 console.log(character);
+
+let i = 0;
 
 let name = "Legolas";
 console.log(name);
@@ -20,3 +24,71 @@ console.log(yes);
 
 let no = false;
 console.log(no);
+
+
+//Exercice 1.1
+
+while(i < 11){
+    console.log(i);
+    i++;
+}
+
+i = 25;
+
+while(i > 19){
+    console.log(i);
+    i--;
+}
+
+i = 0;
+
+while(i < 21){
+    console.log(i);
+    i = i+2;
+}
+
+
+//Exercice 1.2
+i = 0;
+
+while(i < 11){
+    if(i%2 == 0){
+        console.log("Pair");
+    }
+    else
+    {
+        console.log("Impair");
+    }
+    i++;
+}
+
+//Exercice 1.3
+
+i = 0;
+
+while(i < 101)
+{
+    if(i < 51)
+    {
+        if(i%2 == 0){
+            console.log(i);
+            console.log("Pair");
+        }
+    }
+    else
+    {
+        if(i%3 == 0)
+        {
+            console.log(i);
+            if(i%2 == 0){
+                console.log("Pair");
+            }
+            else
+            {
+                console.log("Impair");
+            }
+            
+        }
+    }
+    i++;
+}
---
+++ nb-mystere/index.js
@@ -0,0 +1,100 @@
+/*
+
+Mini projet Le nombre mystère
+Dans votre dossier sites/js/res04-js-j1, créez un nouveau dossier nb-mystere.
+
+Le but du jeu c'est de faire deviner un nombre de votre choix (entre 0 et 100) à l'utilisateur.
+
+Vous allez devoir lui demander de saisir un nombre grâce à window.prompt.
+
+Si le nombre de l'utilisateur est inférieur au votre, utilisez window.alert pour lui dire que c'est plus.
+
+Si le nombre de l'utilisateur est supérieur au votre, utilisez window.alert pour lui dire que c'est moins.
+
+Si son nombre est égal au votre : dites lui qu'il a gagné. Le jeu doit tourner jusqu'à ce que l'utilisateur gagne.
+
+Si vous avez besoin de transformer une chaine de caractère en nombre : utilisez parseInt().
+
+let nb = "42"; // nb est de type String
+let number = parseInt(nb); // number est un Number
+
+*/
+
+let randNum = 0;
+let valeurChoisie = 0;
+let isGoodValue = false;
+let regame = true;
+let answerRegame = "";
+let endgame = false;
+let isGoodRegameAnswer = false;
+
+
+while(regame === true)
+{
+    randNum = Math.floor(Math.random() * 100);
+    window.alert("Trouvez le nombre secret entre 0 et 100");
+    while(endgame === false)
+    {
+        do
+        {
+            valeurChoisie = window.prompt("Quel nombre choisissez-vous?");
+
+            if(isNaN(valeurChoisie)){
+                parseInt(valeurChoisie);
+                if(valeurChoisie < 101 && valeurChoisie > -1){
+                    isGoodValue = true;
+                }
+                else
+                {
+                    window.alert("Vous devez taper un nombre entre 0 et 100");
+                    isGoodValue = false;
+                }
+            }
+            else
+            {
+                window.alert("Vous devez taper un nombre");
+                isGoodValue = false;
+            }
+        }while(!isGoodValue);
+        
+        if(valeurChoisie < randNum)
+        {
+            window.alert("Le nombre recherchéest plus grand que " + valeurChoisie);
+        }
+        else if(valeurChoisie > randNum)
+        {
+            window.alert("Le nombre recherchéest plus petit que " + valeurChoisie);
+        }
+        else
+        {
+            window.alert("Ouiiiiiii! Le nombre recherché est bien " + valeurChoisie);
+            endgame = true;
+        }
+    }
+    
+    
+    if(endgame)
+    {
+        while(!isGoodRegameAnswer)
+        {
+            answerRegame = window.prompt("Voulez-vous rejouer? y/n");
+        
+            if(answerRegame !== 'y' || answerRegame !== 'n')
+            {
+                window.alert("pardon?");
+            
+            }
+            else
+            {
+                isGoodRegameAnswer = true;
+            }
+        }
+        if(answerRegame === 'y'){
+            regame = true;
+        }
+        else
+        {
+            regame = false;
+        }
+    }
+}
