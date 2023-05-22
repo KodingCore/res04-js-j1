183c05aefa9f788df942e2fd85ed6222e05415f5
KodingCore <kcorvais@gmail.com>
2023-05-22 08:14:47 +0000

    fin de l'exo 1 en JS

---
+++ exercice-variables/index.js
@@ -0,0 +1,22 @@
+const character = "Legolas";
+console.log(character);
+
+let name = "Legolas";
+console.log(name);
+name = "Gimli";
+console.log(name);
+
+let nombreGrand = 1515141514645;
+console.log(nombreGrand);
+
+let virgule = 15.15141514645;
+console.log(virgule);
+
+let negatif = -15151;
+console.log(negatif);
+
+let yes = true;
+console.log(yes);
+
+let no = false;
+console.log(no);
